300b151482e71636299fd01ce04f571e43bd663b
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-02-27 16:28:53 +0900

    add:Lineログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :line_users
-  post 'login', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+
   get 'staticpages/top'
 
   get "up" => "rails/health#show", as: :rails_health_check
 
   root "staticpages#top"
   resources :calendars, only: %i[index]
+  
+  devise_for :line_users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks"
+  }
 
 end
