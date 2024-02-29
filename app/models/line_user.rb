6ba430fc734d152c9deee03020119b9362abbae8
Miyaki Fujii <miyaki20030223@icloud.com>
2024-03-01 04:27:13 +0900

    更新

--- app/models/line_user.rb
+++ app/models/line_user.rb
@@ -1,3 +1,14 @@
 class LineUser < ApplicationRecord
-  devise :omniauthable, omniauth_providers: %i[line]
+   validates :provider, presence: true
+   validates :uid, presence: true, uniqueness: { scope: :provider }
+   validates :name, presence: true
+  def self.find_or_create_from_auth_hash(auth_hash)
+    provider = auth_hash[:provider]
+    uid = auth_hash[:uid]
+    name = auth_hash[:info][:name]
+
+    self.find_or_create_by(provider: provider, uid: uid) do |user|
+    user.name = name
+    end
+  end
 end
