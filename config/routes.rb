9576be2a7b02ad9c6d18af4d7b08200270c5886b
Miyaki Fujii <miyaki20030223@icloud.com>
2024-03-09 03:11:17 +0900

    更新

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root "staticpages#top"
 
-  get '/auth/line/callback', to: 'oauth#callback'  
+  get '/auth/line/callback', to: 'oauths#callback'  
 
   get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
 
