542ebbd6b07701612b994a20fb3d0d7789d80705
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-03-09 01:48:59 +0900

    更新

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -13,18 +13,32 @@ class LineBotController < ApplicationController
     events = client.parse_events_from(body)
 
     events.each do |event|
-      case event
-      when Line::Bot::Event::Message
-        case event.type
-        when Line::Bot::Event::MessageType::Text
-          message = {
-            type: 'text',
-            text: event.message['text']
-          }
-          client.reply_message(event['replyToken'], message)
+      if event.type == Line::Bot::Event::MessageType::Text
+        user_message = event.message['text']
+        reply_text = ""
+    
+        Rails.logger.info "Received message: #{user_message}" # ログに受信したメッセージを出力
+    
+        if user_message == "会議"
+          reply_text = "いつリマインドしますか？"
+          Rails.logger.info "Replying with: #{reply_text}" # 応答メッセージをログに出力
+        elsif user_message.match?(/^\d+時$/) # 正規表現で時刻を表すメッセージ（例：「17時」）をチェック
+          # 時刻を指定するメッセージに対する応答
+          reply_text = "#{user_message}に会議のリマインドを設定しました"
+          Rails.logger.info "Replying with: #{reply_text}" # 応答メッセージをログに出力
+        else
+          # その他のメッセージに対するデフォルトの応答
+          reply_text = "ごめんなさい、理解できませんでした。"
+          Rails.logger.info "Replying with: #{reply_text}" # 応答メッセージをログに出力
         end
+    
+        message = {
+          type: 'text',
+          text: reply_text
+        }
+        client.reply_message(event['replyToken'], message)
       end
-    end
+    end    
 
     head :ok
   end
