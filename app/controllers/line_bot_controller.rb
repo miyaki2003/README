3de5c3b9a098f1d266e084d1f45837f76b9faa86
Miyaki Fujii <miyaki20030223@icloud.com>
2024-03-09 22:53:47 +0900

    更新

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,12 +2,19 @@ class LineBotController < ApplicationController
   require 'line/bot'
   skip_before_action :verify_authenticity_token, only: [:callback]
 
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_MESSAGING_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+  end
+
   def callback
     body = request.body.read
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      head :bad_request
+      return head :bad_request
     end
 
     events = client.parse_events_from(body)
@@ -16,37 +23,21 @@ class LineBotController < ApplicationController
       if event.type == Line::Bot::Event::MessageType::Text
         user_message = event.message['text']
         reply_text = ""
-    
-        Rails.logger.info "Received message: #{user_message}"
-    
+
         if user_message == "会議"
           reply_text = "いつリマインドしますか？"
-          Rails.logger.info "Replying with: #{reply_text}" 
-        elsif user_message.match?(/^\d+時$/)
-          reply_text = "#{user_message}に会議のリマインドを設定しました"
-          Rails.logger.info "Replying with: #{reply_text}" 
         else
-          
-          reply_text = "ごめんなさい、理解できませんでした。"
-          Rails.logger.info "Replying with: #{reply_text}"
+          reply_text = "理解できませんでした"
         end
-    
+
         message = {
           type: 'text',
           text: reply_text
         }
         client.reply_message(event['replyToken'], message)
       end
-    end    
+    end
 
     head :ok
-  end
-  private
-
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-    config.channel_secret = ENV["LINE_MESSAGING_CHANNEL_SECRET"]
-    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
   end
 end
