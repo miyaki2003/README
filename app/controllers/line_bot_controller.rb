8b262e43068ab85ad8c1c4a672538dda88191551
Miyaki Fujii <miyaki20030223@icloud.com>
2024-03-12 15:34:13 +0900

    add:LINEbot機能

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -2,16 +2,8 @@ class LineBotController < ApplicationController
   require 'line/bot'
   skip_before_action :verify_authenticity_token, only: [:callback]
 
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_MESSAGING_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
-  end
-
   def callback
     body = request.body.read
-
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
       return head :bad_request
@@ -39,5 +31,51 @@ class LineBotController < ApplicationController
     end
 
     head :ok
+  end
+
+
+
+  events.each do |event|
+    if event.type == Line::Bot::Event::MessageType::Text
+      handle_text_message(event)
+    end
+  end
+
+  head :ok
+end
+
+  private
+
+  def handle_text_message(line_event)
+    user_id = line_event['source']['userId']
+    user_message = line_event.message['text']
+    reply_text = parse_message(user_message)
+
+  if reply_text
+    LineEvent.create(user_id: user_id, message: reply_text[:message], reminder_time: reply_text[:time])
+    response_message = "リマインドを設定しました: #{reply_text[:message]} - #{reply_text[:time].strftime('%Y-%m-%d %H:%M')}"
+  else
+    response_message = "メッセージを理解できませんでした。"
+  end
+
+  message = {
+    type: 'text',
+    text: response_message
+  }
+  client.reply_message(line_event['replyToken'], message)
+end
+  
+  def parse_time(time_str)
+  end
+
+
+
+  
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_MESSAGING_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
   end
 end
---
+++ app/models/line_event.rb
@@ -0,0 +1,2 @@
+class LineEvent < ApplicationRecord
+end
