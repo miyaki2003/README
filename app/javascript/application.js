aa600b1ec052eb2b6f24315581ef6d20d340fc5b
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-03-14 00:24:31 +0900

    更新

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,5 @@
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
-import * as bootstrap from "bootstrap";
 import $ from 'jquery';
-import 'bootstrap';import * as bootstrap from "bootstrap"
+window.jQuery = $;
+window.$ = $;
+
+import "bootstrap";
