5dc8c81978c204e427f3cfc1181be599647edd1d
Miyaki Fujii <miyaki20030223@icloud.com>
2024-03-15 19:11:45 +0900

    更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_172838) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_15_100925) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_14_172838) do
     t.datetime "updated_at", null: false
     t.string "status"
     t.text "temporary_data"
+    t.string "line_user_id"
   end
 
 end
