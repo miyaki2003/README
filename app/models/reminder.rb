2bacd3a57819fad53a05b147c98be8db7256d787
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-03-22 22:17:29 +0900

    更新

---
+++ app/models/reminder.rb
@@ -0,0 +1,11 @@
+class Reminder < ApplicationRecord
+  def change
+    create_table :reminders do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.text :message
+      t.datetime :reminder_time
+
+      t.timestamps
+    end
+end
---
+++ db/migrate/20240322073456_create_reminders.rb
@@ -0,0 +1,12 @@
+class CreateReminders < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reminders do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.text :message
+      t.datetime :reminder_time
+    
+      t.timestamps
+    end
+  end
+end
