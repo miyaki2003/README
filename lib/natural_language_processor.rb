7f584c959f4ae46b912ce4ffbb4a0a22bfc0c636
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-03-22 14:31:44 +0900

    update:LINEbotの変更

--- lib/natural_language_processor.rb
+++ lib/natural_language_processor.rb
@@ -2,7 +2,8 @@ require 'google/cloud/language'
 
 class NaturalLanguageProcessor
   def self.parse_time_from_text(text)
-    language_service = Google::Cloud::Language.language_service
+    credentials = JSON.parse(ENV['GOOGLE_CREDENTIALS'], symbolize_names: true)
+    language_service = Google::Cloud::Language.language_service(credentials: credentials)
 
     document = { content: text, type: :PLAIN_TEXT }
     response = language_service.analyze_entities document: document
