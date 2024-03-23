570376567aeebbdde193fb444ca491af32f98bdf
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-03-23 10:36:25 +0900

    更新

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,11 +1,3 @@
 class Reminder < ApplicationRecord
-  def change
-    create_table :reminders do |t|
-      t.references :user, null: false, foreign_key: true
-      t.string :title
-      t.text :message
-      t.datetime :reminder_time
-
-      t.timestamps
-    end
+  belongs_to :user
 end
