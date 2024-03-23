6a836ebb8b3b03c7688ff035e3da622d762952b4
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-03-23 10:21:30 +0900

    更新

---
+++ app/jobs/reminder_job.rb
@@ -0,0 +1,19 @@
+class ReminderJob < ApplicationJob
+  queue_as :default
+
+  def perform(reminder_id)
+    reminder = Reminder.find_by(id: reminder_id)
+    return unless reminder
+
+    message = {
+      type: 'text',
+      text: "#{reminder.title}の時間です"
+    }
+    client = Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
+    response = client.push_message(reminder.user.line_user_id, message)
+    Rails.logger.info("Sent reminder: #{response}")
+  end
+end
---
+++ app/services/reminder_service.rb
@@ -0,0 +1,11 @@
+class ReminderService
+  def self.create(user:, title:, reminder_time:)
+    reminder = user.reminders.build(title: title, reminder_time: reminder_time)
+    
+    if reminder.save
+      ReminderJob.set(wait_until: reminder.reminder_time).perform_later(reminder.id)
+    end
+
+    reminder
+  end
+end
