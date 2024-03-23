811fea3f43f32bd90903f4e809501cb972d2832c
Miyaki Fujii <miyaki20030223@icloud.com>
2024-03-24 01:16:03 +0900

    更新

--- lib/natural_language_processor.rb
+++ lib/natural_language_processor.rb
@@ -1,14 +1,12 @@
-require 'google/cloud/language'
+require "google/cloud/language"
 
 class NaturalLanguageProcessor
   def self.parse_time_from_text(text)
-    credentials = JSON.parse(ENV['GOOGLE_CREDENTIALS'], symbolize_names: true)
-    language_service = Google::Cloud::Language.language_service(credentials: credentials)
-
+    project_id = "instant-subset-417305"
+    language = Google::Cloud::Language.language_service
     document = { content: text, type: :PLAIN_TEXT }
-    response = language_service.analyze_entities document: document
-
-    datetime_entity = response.entities.find { |entity| entity.type == :DATE_TIME }
-    datetime_entity&.metadata['value']
+    response = language.analyze_entities document: document
+    datetime_entities = response.entities.select { |entity| entity.type == "DATE" || entity.type == "NUMBER" }
+    datetime_entities.map { |entity| entity.metadata['value'] }.compact
   end
 end
