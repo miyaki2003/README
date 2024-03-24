b4ad16f1dc10d725cb068faf79e711b17b5082fd
Miyaki Fujii <miyaki20030223@icloud.com>
2024-03-25 02:27:51 +0900

    モデルの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   authenticates_with_sorcery!
   has_many :authentications, dependent: :destroy
   accepts_nested_attributes_for :authentications
+  has_many :line_events, dependent: :destroy
+  has_many :reminders, dependent: :destroy
 end
