c079227291a6ad7d573f3b0afaa63e7e291d82b4
Miyaki Fujii <miyaki20030223@icloud.com>
2024-03-25 10:48:42 +0900

    認証キーの変更

--- lib/natural_language_processor.rb
+++ lib/natural_language_processor.rb
@@ -1,13 +1,31 @@
-require "google/cloud/language"
+require 'net/http'
+require 'uri'
+require 'json'
+
 class NaturalLanguageProcessor
   def self.parse_time_from_text(text)
-    credentials = JSON.parse(ENV['GOOGLE_CREDENTIALS'], symbolize_names: true)
-    language_service = Google::Cloud::Language.language_service(credentials: credentials)
+    api_key = ENV['GOOGLE_API_KEY']
 
-    document = { content: text, type: :PLAIN_TEXT }
-    response = language_service.analyze_entities document: document
+    uri = URI.parse("https://language.googleapis.com/v1/documents:analyzeEntities?key=#{api_key}")
 
-    datetime_entity = response.entities.find { |entity| entity.type == :DATE_TIME }
+    request_body = {
+      document: {
+        type: "PLAIN_TEXT",
+        content: text
+      },
+      encodingType: "UTF8"
+    }.to_json
+
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = "application/json"
+    request.body = request_body
+
+    response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == "https") do |http|
+      http.request(request)
+    end
+
+    response_body = JSON.parse(response.body, symbolize_names: true)
+    datetime_entity = response_body[:entities].find { |entity| entity[:type] == "DATE" || entity[:type] == "TIME" }
     datetime_entity&.metadata['value']
   end
 end
