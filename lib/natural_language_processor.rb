c36cd40781f88634f52bc72b09cc463a900ed987
Miyaki Fujii <miyaki20030223@icloud.com>
2024-03-27 02:30:33 +0900

    動作確認

--- lib/natural_language_processor.rb
+++ lib/natural_language_processor.rb
@@ -7,16 +7,17 @@ class NaturalLanguageProcessor
   def self.translate_japanese_to_english(text)
     if text =~ /今日の(\d+)時(\d*)分?/
       hour = $1.to_i
-      minutes = $2.empty? ? '00' : $2
-      "today at #{format('%02d', hour)}:#{minutes}"
+      minutes = $2.empty? ? '0' : $2
+      "today at #{format('%d', hour)}:#{minutes}"
     end
   end
 
   def self.parse_time_from_text(text)
     translated_text = translate_japanese_to_english(text)
     datetime = Chronic.parse(translated_text)
+    Rails.logger.debug "Parsed Time: #{datetime}, Time Zone: #{datetime.zone}"
     if datetime
-      datetime.strftime('%Y-%m-%d %H:%M:%S')
+      datetime.strftime('%Y-%m-%d %H:%M')
     else
       nil
     end
