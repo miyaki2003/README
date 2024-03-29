208a88882f047133d46310dbf474b14628ffff17
Miyaki Fujii <miyaki20030223@icloud.com>
2024-03-30 02:01:19 +0900

    active_supportの使用

--- lib/natural_language_processor.rb
+++ lib/natural_language_processor.rb
@@ -1,11 +1,15 @@
-require 'net/http'
-require 'uri'
-require 'json'
-require 'chronic'
-require 'date'
-require 'time'
-
+require 'active_support/all'
 class NaturalLanguageProcessor
+  DAY_MAPPINGS = {
+      /日曜?日?/ => 0,
+      /月曜?日?/ => 1,
+      /火曜?日?/ => 2,
+      /水曜?日?/ => 3,
+      /木曜?日?/ => 4,
+      /金曜?日?/ => 5,
+      /土曜?日?/ => 6
+    }.freeze
+
   def self.parse_and_format_datetime(text)
     case text
     when /(今日|明日|明後日)の?(朝|午前|午後)?(\d+)(?:時|:)(\d*)分?/
@@ -15,31 +19,78 @@ class NaturalLanguageProcessor
     when /(\d+)分後/, /(\d+)時間後/, /(\d+)日後/, /(\d+)週間後/, /(\d+)ヶ月後/
       translate_relative_time(text)
     else
-      translate_weekday_and_relative_week(text)
+      day_match = text.match(/(今週|来週|再来週)の?(日|月|火|水|木|金|土)(曜?日?)?/)
+      time_match = text.match(/(\d+)(?:時|:)(\d*)分?/)
+      period_match = text.match(/(朝|午前|午後)/)
+      translate_weekday_and_relative_week(day_match, time_match, period_match) if day_match
     end
   end
-  
+
   private
 
   def self.translate_relative_day_time(day, period, hour, minutes)
     date = case day
-           when "今日" then Date.today
-           when "明日" then Date.today + 1
-           when "明後日" then Date.today + 2
-           else Date.today
+           when "今日" then Time.current
+           when "明日" then 1.day.since
+           when "明後日" then 2.days.since
+           else Time.current
            end
     hour = adjust_hour_for_period(hour.to_i, period)
-    "#{date} at #{format('%02d', hour)}:#{format('%02d', minutes.to_i)}"
+    "#{date.strftime('%Y-%m-%d')} at #{format('%02d', hour)}:#{format('%02d', minutes.to_i)}"
   end
 
   def self.translate_specific_date_time(month, day, period, hour, minutes)
-    year = Date.today.year
+    year = Time.current.year
     hour = adjust_hour_for_period(hour.to_i, period)
-    "#{year}-#{format('%02d', month)}-#{format('%02d', day)} at #{format('%02d', hour)}:#{format('%02d', minutes.to_i)}"
+    date = Time.new(year, month, day, hour, minutes.to_i)
+    date.strftime('%Y-%m-%d at %H:%M')
+  end
+
+  def self.translate_relative_time(text)
+    case text
+    when /(\d+)分後/
+      minutes = $1.to_i
+      time = Time.current + minutes.minutes
+    when /(\d+)時間後/
+      hours = $1.to_i
+      time = Time.current + hours.hours
+    when /(\d+)日後/
+      days = $1.to_i
+      time = Time.current + days.days
+    when /(\d+)週間後/
+      weeks = $1.to_i
+      time = Time.current + weeks.weeks
+    when /(\d+)ヶ月後/
+      months = $1.to_i
+      time = Time.current + months.months
+    else
+      return "Unrecognized format"
+    end
+    time.strftime('%Y-%m-%d %H:%M:%S')
+  end
+
+  def self.translate_weekday_and_relative_week(day_match, time_match, period_match)
+    week_modifier = case day_match[1]
+                    when "今週" then 0.weeks
+                    when "来週" then 1.week
+                    when "再来週" then 2.weeks
+                    end
+    wday_key = day_match[2]
+    DAY_MAPPINGS.each do |pattern, value|
+      if wday_key.match(pattern)
+        wday = value
+        break
+      end
+    end
+    target_date = Time.current.beginning_of_week(:sunday) + wday.days + week_modifier
+    hour = time_match ? time_match[1].to_i : 6
+    minute = time_match && time_match[2] ? time_match[2].to_i : 0
+    hour = adjust_hour_for_period(hour, period_match ? period_match[1] : nil)
+    
+    target_date.change(hour: hour, min: minute)
   end
 
   def self.adjust_hour_for_period(hour, period)
-    return hour if period.nil?
     if period == "午後" && hour < 12
       hour + 12
     elsif (period == "午前" || period == "朝") && hour == 12
@@ -49,65 +100,11 @@ class NaturalLanguageProcessor
     end
   end
 
-  def self.translate_relative_time(text)
-    case text
-    when /(\d+)分後/
-      minutes = $1.to_i
-      time = Time.now + (minutes * 60)
-    when /(\d+)時間後/
-      time = Time.now + (60 * 60)
-    when /(\d+)日後/
-      time = Time.now + (24 * 60 * 60)
-    when /(\d+)週間後/
-      time = Time.now + (7 * 24 * 60 * 60)
-    when /(\d+)ヶ月後/
-      time = Time.now + (30 * 24 * 60 * 60)
-    else
-      return "Unrecognized format"
-    end
-    time.strftime('%Y-%m-%d %H:%M:%S')
-  end
-
-  def self.translate_weekday_and_relative_week(text)
-    day_english = {
-      /日曜?日?/ => "Sunday",
-      /月曜?日?/ => "Monday",
-      /火曜?日?/ => "Tuesday",
-      /水曜?日?/ => "Wednesday",
-      /木曜?日?/ => "Thursday",
-      /金曜?日?/ => "Friday",
-      /土曜?日?/ => "Saturday"
-    }
-
-    time_match = text.match(/(\d+)(?:時|:)(\d*)分?/)
-    hour = time_match ? time_match[1].to_i : 6
-    minute = time_match && !time_match[2].empty? ? time_match[2].to_i : 0
-
-    period_match = text.match(/(朝|午前|午後)/)
-    period = period_match ? period_match[1] : nil
-
-    adjusted_hour = adjust_hour_for_period(hour, period)
-
-    text.gsub!(/(今週|来週|再来週)の?(朝|午前|午後)?(.+)/) do |match|
-      period_keyword, time_of_day, day_japanese = $1, $2, $3
-      day_english_value = day_english.find { |k, _| day_japanese =~ k }.last
-      period_english = case period_keyword
-                        when "今週" then "this"
-                        when "来週" then "next"
-                        when "再来週" then "in two weeks"
-                        end
-      formatted_hour = format('%02d', hour)
-      formatted_minute = format('%02d', minute)
-      "#{period_english} #{day_english_value} #{formatted_hour}:#{formatted_minute}"
-    end
-  end
-
   def self.parse_time_from_text(text)
     translated_text = parse_and_format_datetime(text)
-    datetime = Chronic.parse(translated_text)
-    if datetime
-      datetime.strftime('%Y-%m-%d %H:%M')
-    else
+    begin
+      DateTime.parse(translated_text)
+    rescue ArgumentError
       nil
     end
   end
