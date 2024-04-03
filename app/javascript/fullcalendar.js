99908fa2587aa3e3cf351ced1e23ada5a8d44478
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-04-04 02:20:54 +0900

    calendarとlinebotの修正

--- app/javascript/fullcalendar.js
+++ app/javascript/fullcalendar.js
@@ -3,6 +3,7 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
 import bootstrap5Plugin from '@fullcalendar/bootstrap5';
+import jaLocale from '@fullcalendar/core/locales/ja';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap-icons/font/bootstrap-icons.css';
@@ -13,12 +14,19 @@ document.addEventListener('DOMContentLoaded', function() {
     let calendar = new Calendar(calendarEl, {
       plugins: [ dayGridPlugin, timeGridPlugin, listPlugin, bootstrap5Plugin ],
       themeSystem: 'bootstrap5',
-      initialView: 'dayGridMonth',
+      locale: 'ja',
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,listWeek'
-      }
+      },
+      buttonText: {
+        today: '今月',
+        month: '月',
+        week: '週',
+        list: 'リスト'
+      },
+      initialView: 'dayGridMonth',
     });
     calendar.render();
   }
