d8afef2fb010baeef068e70d0626b35e97ab7a68
Miyaki Fujii <miyaki20030223@icloud.com>
2024-04-03 22:04:03 +0900

    add:取り消しの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_073456) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_075526) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_22_073456) do
     t.datetime "reminder_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_active", default: true, null: false
     t.index ["user_id"], name: "index_reminders_on_user_id"
   end
 
