36042aaa1d9faaffff8f1a4757dd0b831ae9c15a
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-04-19 12:21:38 +0900

    更新

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -6,27 +6,16 @@ class OauthsController < ApplicationController
   
   def callback
     provider = "line"
-    omniauth_data = request.env['omniauth.auth']
-    
-    if omniauth_data.nil?
-      redirect_to root_path, alert: "認証データが取得できませんでした。"
-      return
-    end
-
     if @user = login_from(provider)
-      unless @user.line_user_id == omniauth_data['uid']
-        @user.update(line_user_id: omniauth_data['uid'])
-      end
-      redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+        redirect_to root_path, notice: "#{provider.titleize}でログインしました。"
     else
-      @user = build_from(provider)
-      @user.line_user_id = omniauth_data['uid']
+      @user = create_from(provider)
       reset_session
       auto_login(@user)
-      if @user.save
-        redirect_to root_path, notice: "#{provider.titleize}でログインしました"
+      if @user.persisted?
+        redirect_to root_path, notice: "#{provider.titleize}でログインしました。"
       else
-        redirect_to root_path, alert: "ログインに失敗しました"
+        redirect_to root_path, alert: "ログインに失敗しました。"
       end
     end
   end
@@ -38,7 +27,7 @@ class OauthsController < ApplicationController
 
   private
 
-  def auth_params
-    params.permit(:code, :provider, :error, :state)
-  end
+    def auth_params
+        params.permit(:code, :provider, :error, :state)
+    end
 end
