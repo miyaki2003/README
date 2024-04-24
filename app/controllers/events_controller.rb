b83578c66015a44a3fcc9ff36b20dcb5bff64a28
Miyaki Fujii <miyaki20030223@icloud.com>
2024-04-24 19:39:43 +0900

    更新

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,8 +5,8 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = current_user.events.build(event_params)
-    #@event = Event.new(event_params)
+    #@event = current_user.events.build(event_params)
+    @event = Event.new(event_params)
 
     @event.line_user_id = current_user.line_user_id
     set_datetime_params
