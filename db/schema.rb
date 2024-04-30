81d5a16610e95a297578b5627e5790745831c0c5
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-04-30 23:13:01 +0900

    利用規約作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_19_055020) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_30_132232) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_19_055020) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.string "line_user_id"
+    t.date "event_date"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
