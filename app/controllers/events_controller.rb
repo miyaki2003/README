4c254ec105017d04c409a1c8e670076e54289168
Miyaki Fujii <miyaki20030223@icloud.com>
2024-04-30 22:14:41 +0900

    更新

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,7 +69,7 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:title, :line_notify, :start_date, :start_time, :end_date, :end_time, :notify_date, :notify_time)
+    params.require(:event).permit(:title, :line_notify, :event_date, :start_date, :start_time, :end_date, :end_time, :notify_date, :notify_time)
   end
 
   def set_datetime_params
