5a0c0dbf57012849f67c32510cc85ed57bc38e62
藤井京樹 <fujiimiyaki@fujiimiyaki.local>
2024-05-14 02:22:41 +0900

    更新

--- app/services/reminder_service.rb
+++ app/services/reminder_service.rb
@@ -1,24 +1,11 @@
 class ReminderService
-  def self.create(user:, title: nil, reminder_time:, image_id: nil, reminder_type: 'text')
-
-    if reminder_type == 'image' && image_id.nil?
-      raise ArgumentError, "Image ID is required for image reminders"
-    elsif reminder_type == 'text' && title.nil?
-      raise ArgumentError, "Title is required for text reminders"
-    end
-
-    if reminder_type == 'image'
-      reminder = user.reminders.build(image_id: image_id, reminder_time: reminder_time, reminder_type: reminder_type)
-    else
-      reminder = user.reminders.build(title: title, reminder_time: reminder_time, reminder_type: reminder_type)
-    end
+  def self.create(user:, title:, reminder_time:)
+    reminder = user.reminders.build(title: title, reminder_time: reminder_time)
     
     if reminder.save
       ReminderJob.set(wait_until: reminder.reminder_time).perform_later(reminder.id)
-    else
-      Rails.logger.error("Failed to save reminder: #{reminder.errors.full_messages.join(", ")}")
-      return nil
     end
+
     reminder
   end
 end
