f3e80bb26fa728d64899baaacb01f8c5d62cc3e9
藤井京樹 <miyaki20030223@icloud.com>
2024-05-17 06:52:32 +0900

    更新

--- app/jobs/Notification_job.rb
+++ app/jobs/Notification_job.rb
@@ -6,23 +6,15 @@ class NotificationJob < ApplicationJob
     event = Event.find_by(id: event_id)
     return unless event && event.user.line_user_id.present?
     message_text = "「#{event.title}」のリマインドです"
-    Rails.logger.info "Sending LINE notification to #{event.user.line_user_id} with message: #{message_text}"
     LineNotifyService.send_message(event.user.line_user_id, message_text)
-    Rails.logger.info "LINE notification sent"
-  rescue => e
-    Rails.logger.error "Failed to send LINE notification: #{e.message}"
-    Rails.logger.error e.backtrace.join("\n")
   end
 
   def self.cancel(job_id)
-    Rails.logger.info "Attempting to cancel job with ID: #{job_id}"
     scheduled_set = Sidekiq::ScheduledSet.new
     job = scheduled_set.find { |j| j.jid == job_id }
     if job
       job.delete
-      Rails.logger.info "Job with ID: #{job_id} cancelled successfully"
     else
-      Rails.logger.info "Job with ID: #{job_id} not found"
     end
   end
 end
