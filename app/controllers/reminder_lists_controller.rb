50fe3ee6fa9cdcfdd4ab6408d8db0929145e6d40
藤井京樹 <miyaki20030223@icloud.com>
2024-05-22 17:27:10 +0900

    更新

--- app/controllers/reminder_lists_controller.rb
+++ app/controllers/reminder_lists_controller.rb
@@ -3,18 +3,8 @@ class ReminderListsController < ApplicationController
     @reminders = current_user.reminders.where("is_active = ? AND reminder_time > ?", true, Time.now).order(reminder_time: :asc)
   end
 
-  # def deactivate
-  #   @reminder = current_user.reminders.find(params[:id])
-  #   if @reminder.update(is_active: false)
-  #     redirect_to reminder_lists_path
-  #   else
-  #     redirect_to reminder_lists_path
-  #   end
-  # end
-
   def deactivate
     @reminder = current_user.reminders.find(params[:id])
-    puts "Deactivating reminder with ID: #{params[:id]}"
     if @reminder.update(is_active: false)
       render json: { success: true }, status: :ok
     else
