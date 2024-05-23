dfc6136ca23092d127082a180d401889eb5ccae1
藤井京樹 <miyaki20030223@icloud.com>
2024-05-23 17:54:19 +0900

    job変更

--- app/jobs/Notification_job.rb
+++ app/jobs/Notification_job.rb
@@ -11,37 +11,14 @@ class NotificationJob < ApplicationJob
     Rails.logger.info "LINE notification sent for event ID: #{event_id}"
   end
 
-  def self.cancel(job_id, event_id)
-
-    event = Event.find_by(id: event_id)
-    unless event
-      Rails.logger.error "イベントが見つかりませんでした: #{event_id}"
-      return
-    end
-
-    Rails.logger.info "データベースのジョブID: #{event.notification_job_id}, 渡されたジョブID: #{job_id}"
-
-    if event.notification_job_id != job_id
-      Rails.logger.error "データベースのジョブIDと渡されたジョブIDが一致しません: #{event.notification_job_id} != #{job_id}"
-      return
-    end
-
+  def self.cancel(job_id)
     scheduled_set = Sidekiq::ScheduledSet.new
-    Rails.logger.info "Scheduled jobs in Sidekiq:"
-    scheduled_set.each do |job|
-      enqueued_at = job.enqueued_at ? Time.at(job.enqueued_at) : 'nil'
-      Rails.logger.info "Job ID: #{job.jid}, Class: #{job.klass}, Args: #{job.args}, Enqueued At: #{enqueued_at}"
-    end
-    
-    job = scheduled_set.find { |j| j.jid == job_id }
-    Rails.logger.info "取得したジョブ: #{job.inspect}"
-
+    job = scheduled_set.find { |j| j.args.any? { |arg| arg["job_id"] == job_id } }
     unless job
       Rails.logger.error "ジョブが見つかりませんでした"
-      return
     end
-
-    job.delete
-    Rails.logger.info "ジョブがキャンセルされました: #{job_id}"
-  end
+    if job
+      job.delete
+    end
+  end  
 end
