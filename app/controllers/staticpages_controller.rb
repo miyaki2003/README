ca4926e3e36ddfaa7ba4da2469d35a0b1bbb3775
藤井京樹 <miyaki20030223@icloud.com>
2024-05-24 17:34:23 +0900

    プライバシー更新

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -2,4 +2,8 @@ class StaticpagesController < ApplicationController
   skip_before_action :require_login, only: %i[top]
   def top
   end
+  def privacy_policy
+    html_content = render_to_string(partial: 'privacy_policy', layout: false)
+    render json: { html: html_content }, status: :ok
+  end
 end
