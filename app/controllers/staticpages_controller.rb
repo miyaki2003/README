146e12ef4c5421af1fc68301e009c50383a981ef
藤井京樹 <miyaki20030223@icloud.com>
2024-05-24 20:54:46 +0900

    更新

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -3,7 +3,7 @@ class StaticpagesController < ApplicationController
   def top
   end
   def privacy_policy
-    html_content = render_to_string(template: 'staticpages/privacy_policy', layout: false)
+    html_content = render_to_string(template: 'staticpages/privacy_policy', layout: false, formats: [:html])
     render json: { html: html_content }, status: :ok
   end
 end
