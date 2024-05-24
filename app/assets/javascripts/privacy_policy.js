fda1c356d002519a73552cfd16eadab714079292
藤井京樹 <miyaki20030223@icloud.com>
2024-05-24 21:11:22 +0900

    更新

--- app/assets/javascripts/privacy_policy.js
+++ app/assets/javascripts/privacy_policy.js
@@ -1,34 +1,38 @@
 document.addEventListener('DOMContentLoaded', function() {
-  const originalContent = document.querySelector('#modal-content').innerHTML;
-  const privacyPolicyLink = document.querySelector('a[href="/staticpages/privacy_policy"]');
-  const settingEventModal = document.querySelector('#settingEventModal');
+  const privacyPolicyLink = document.getElementById('privacy-policy-link');
   const modalContent = document.querySelector('#modal-content');
-  
-  privacyPolicyLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    fetch('/staticpages/privacy_policy', {
-      method: 'GET',
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest'
-      }
-    })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error('Network response was not ok.');
-      }
-    })
-    .then(data => {
-      modalContent.innerHTML = '';
-      modalContent.innerHTML = data.html;
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch operation:', error);
-    });
-  });
+  const settingEventModal = document.querySelector('#settingEventModal');
+  const originalContent = modalContent.innerHTML;
 
-  settingEventModal.addEventListener('hidden.bs.modal', function () {
-    modalContent.innerHTML = originalContent;
-  });
+  if (privacyPolicyLink) {
+    privacyPolicyLink.addEventListener('click', function(event) {
+      event.preventDefault();
+      fetch('/staticpages/privacy_policy', {
+        method: 'GET',
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest'
+        }
+      })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error('Network response was not ok.');
+        }
+      })
+      .then(data => {
+        modalContent.innerHTML = '';
+        modalContent.innerHTML = data.html;
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+    });
+  }
+
+  if (settingEventModal) {
+    settingEventModal.addEventListener('hidden.bs.modal', function () {
+      modalContent.innerHTML = originalContent;
+    });
+  }
 });
