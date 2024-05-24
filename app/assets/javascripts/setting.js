d7212b8a553856c73da9e3f6df4a9d718cae7fa4
藤井京樹 <miyaki20030223@icloud.com>
2024-05-25 02:40:24 +0900

    コード修正

--- app/assets/javascripts/setting.js
+++ app/assets/javascripts/setting.js
@@ -3,71 +3,71 @@ document.addEventListener('DOMContentLoaded', function() {
   const settingEventModal = document.querySelector('#settingEventModal');
   const originalContent = modalContent.innerHTML;
 
-  function attachPrivacyPolicyListener() {
+  async function fetchData(url) {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'X-Requested-With': 'XMLHttpRequest'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('Network response was not ok.');
+    }
+    return response;
+  }
+
+  async function loadContent(link, container) {
+    try {
+      const response = await fetchData(link);
+      if (link === '/') {
+        const html = await response.text();
+        container.innerHTML = html;
+      } else {
+        const data = await response.json();
+        container.innerHTML = data.html;
+      }
+      attachLinkListeners();
+    } catch (error) {
+      console.error('There was a problem with the fetch operation:', error);
+    }
+  }
+
+  function attachLinkListeners() {
     const termsLink = document.getElementById('terms-link');
     const privacyPolicyLink = document.getElementById('privacy-policy-link');
-    
+    const topLink = document.getElementById('top-link');
+
     if (termsLink) {
       termsLink.addEventListener('click', function(event) {
         event.preventDefault();
-        fetch('/staticpages/terms', {
-          method: 'GET',
-          headers: {
-            'X-Requested-With': 'XMLHttpRequest'
-          }
-        })
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error('Network response was not ok.');
-          }
-        })
-        .then(data => {
-          modalContent.innerHTML = '';
-          modalContent.innerHTML = data.html;
-          attachPrivacyPolicyListener();
-        })
-        .catch(error => {
-          console.error('There was a problem with the fetch operation:', error);
-        });
+        loadContent('/staticpages/terms', modalContent);
       });
     }
 
     if (privacyPolicyLink) {
       privacyPolicyLink.addEventListener('click', function(event) {
         event.preventDefault();
-        fetch('/staticpages/privacy_policy', {
-          method: 'GET',
-          headers: {
-            'X-Requested-With': 'XMLHttpRequest'
-          }
-        })
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error('Network response was not ok.');
-          }
-        })
-        .then(data => {
-          modalContent.innerHTML = '';
-          modalContent.innerHTML = data.html;
-          attachPrivacyPolicyListener();
-        })
-        .catch(error => {
-          console.error('There was a problem with the fetch operation:', error);
-        });
+        loadContent('/staticpages/privacy_policy', modalContent);
+      });
+    }
+
+    if (topLink) {
+      topLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        loadContent('/', document.getElementById('main-content'));
+        const bootstrapModal = bootstrap.Modal.getInstance(settingEventModal);
+        bootstrapModal.hide();
+        history.pushState(null, null, '/');
       });
     }
   }
 
-  attachPrivacyPolicyListener();
+  attachLinkListeners();
 
   if (settingEventModal) {
     settingEventModal.addEventListener('hidden.bs.modal', function () {
       modalContent.innerHTML = originalContent;
-      attachPrivacyPolicyListener();
+      attachLinkListeners();
     });
   }
 });
