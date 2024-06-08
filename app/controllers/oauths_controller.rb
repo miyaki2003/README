4f48088e9890a5519c0caa4ae8acec528c499dd6
藤井京樹 <miyaki20030223@icloud.com>
2024-06-08 22:42:32 +0900

    add:トップページイラスト

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -1,5 +1,5 @@
 class OauthsController < ApplicationController
-  skip_before_action :require_login, only: [:oauth, :callback, :destroy]
+  # skip_before_action :require_login, only: [:oauth, :callback, :destroy]
 
   require 'net/http'
   require 'uri'
