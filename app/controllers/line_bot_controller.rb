60336c2ea56d9c46d443546b228f28284ce6ea8d
藤井京樹 <miyaki20030223@icloud.com>
2024-06-12 16:44:22 +0900

    天気レイアウト修正

--- app/controllers/line_bot_controller.rb
+++ app/controllers/line_bot_controller.rb
@@ -288,14 +288,14 @@ class LineBotController < ApplicationController
           },
           {
             type: 'box',
-            layout: 'baseline',
+            layout: 'vertical',
             contents: [
               {
                 type: 'text',
                 text: "#{weather} #{weather_emoji(weather)}",
                 size: 'md',
                 color: '#111111',
-                align: 'center'
+                align: 'start'
               }
             ],
             spacing: 'md',
@@ -306,7 +306,7 @@ class LineBotController < ApplicationController
             text: "気温: #{temperature}°C",
             size: 'md',
             color: '#555555',
-            align: 'center',
+            align: 'start',
             margin: 'md'
           },
           {
@@ -314,7 +314,7 @@ class LineBotController < ApplicationController
             text: "降水確率: #{precipitation_probability}%",
             size: 'md',
             color: '#555555',
-            align: 'center',
+            align: 'start',
             margin: 'md'
           },
           {
@@ -322,7 +322,7 @@ class LineBotController < ApplicationController
             text: "降水量: #{rainfall} mm",
             size: 'md',
             color: '#555555',
-            align: 'center',
+            align: 'start',
             margin: 'md'
           },
           {
@@ -330,7 +330,7 @@ class LineBotController < ApplicationController
             text: "風速: #{wind_speed} m/s",
             size: 'md',
             color: '#555555',
-            align: 'center',
+            align: 'start',
             margin: 'md'
           },
           {
@@ -338,7 +338,7 @@ class LineBotController < ApplicationController
             text: "湿度: #{humidity}%",
             size: 'md',
             color: '#555555',
-            align: 'center',
+            align: 'start',
             margin: 'md'
           }
         ],
